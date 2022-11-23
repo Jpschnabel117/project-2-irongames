c2720ccc2546257059735d3b86ad66f9b1f8bd2f
Jpschnabel177 <jpschnabel96@gmail.com>
2022-11-23 17:39:15 -0500

    working on file tree

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express  = require("hbs");
+const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -7,6 +7,28 @@ const app = express();
 require("./config/session.config")(app);
 require("dotenv/config");
 
-app.set("views",path.join(__dirname, "views"));
-app.set("view engine","hbs")
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// const indexRouter = require("./routes/index");
+// app.use("/", indexRouter);
+
+// const authRouter = require("./routes/auth.route");
+// app.use("/auth", authRouter);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+require("./error-handling")(app);
+
+
+module.exports = app
